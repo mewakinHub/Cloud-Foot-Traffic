b4ea5658a8fd35eeb7012d6f74c7c112e4b077f0
ranvie <neodyte@gmail.com>
2024-11-23 13:34:46 +0700

    huh2

---
+++ web-app/next-js/src/app/results/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React, { useState } from 'react';
+import api from '../../utils/api';
+
+const GetData: React.FC = () => {
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default GetData;
